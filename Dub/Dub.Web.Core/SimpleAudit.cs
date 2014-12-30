ebe2f2b5897061480ec4ec46e341575fed90413d
Andrey Kurdyumov <kant2002@gmail.com>
2014-12-30 18:24:32 +0600

    Add simple aut

---
+++ Dub/Dub.Web.Core/SimpleAudit.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SimpleAudit.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Core
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Complex type which describe simple audit entities in the database.
+    /// </summary>
+    [ComplexType]
+    public class SimpleAudit
+    {
+        /// <summary>
+        /// Gets or sets user name who change entity.
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets date when entity was created.
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets user who last time modify entity.
+        /// </summary>
+        public string ModifiedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets date when entity was last time modified.
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+    }
+}
