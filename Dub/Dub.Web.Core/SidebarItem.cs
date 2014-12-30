c53882f465160fc77e0670ce1ba1aedecb8777e0
Andrey Kurdyumov <kant2002@gmail.com>
2014-12-30 18:04:34 +0600

    Added

---
+++ Dub/Dub.Web.Core/SidebarItem.cs
@@ -0,0 +1,69 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SidebarItem.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Core
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Side bar item.
+    /// </summary>
+    public class SidebarItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SidebarItem"/> class.
+        /// </summary>
+        public SidebarItem()
+        {
+            this.Items = new List<SidebarItem>();
+        }
+
+        /// <summary>
+        /// Gets or sets generic id for the sidebar item.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets icon for the sidebar item.
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// Gets or sets title for the sidebar.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this element is active.
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this element is opened.
+        /// </summary>
+        public bool IsOpen { get; set; }
+
+        /// <summary>
+        /// Gets or sets MVC action for the action.
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// Gets or sets MVC controller for the action.
+        /// </summary>
+        public string Controller { get; set; }
+
+        /// <summary>
+        /// Gets or sets route parameters for the MVC action.
+        /// </summary>
+        public object RouteParameters { get; set; }
+
+        /// <summary>
+        /// Gets list of child items
+        /// </summary>
+        public List<SidebarItem> Items { get; private set; }
+    }
+}
---
+++ Dub/Dub.Web.Core/SidebarManagerBase.cs
@@ -0,0 +1,105 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SidebarManager.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Core
+{
+    using System;
+    using System.Security.Claims;
+
+    /// <summary>
+    /// Manages the sidebar.
+    /// </summary>
+    public class SidebarManagerBase : IDisposable
+    {
+        /// <summary>
+        /// Root sidebar item.
+        /// </summary>
+        private SidebarItem rootItem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SidebarManagerBase"/> class.
+        /// </summary>
+        /// <param name="userBuilder">User for which sidebar manager will provide results.</param>
+        public SidebarManagerBase(Func<ClaimsPrincipal> userBuilder)
+        {
+            this.User = new Lazy<ClaimsPrincipal>(userBuilder);
+        }
+
+        /// <summary>
+        /// Gets user for which sidebar manager will provide information.
+        /// </summary>
+        public Lazy<ClaimsPrincipal> User { get; private set; }
+
+        /// <summary>
+        /// Get root item for sidebar.
+        /// </summary>
+        /// <returns>Root sidebar items.</returns>
+        public SidebarItem GetRootSidebarItem()
+        {
+            if (this.rootItem != null)
+            {
+                return this.rootItem;
+            }
+
+            var rootItem = new SidebarItem();
+            this.PopulateRootItem(rootItem);
+            this.rootItem = rootItem;
+            return rootItem;
+        }
+
+        /// <summary>
+        /// Get sidebar item by id.
+        /// </summary>
+        /// <param name="id">Id of the sidebar item to find</param>
+        /// <returns>Sidebar item with requested id if exists, null otherwise.</returns>
+        public SidebarItem FindById(string id)
+        {
+            var root = this.GetRootSidebarItem();
+            return FindById(id, root);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or
+        /// resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Populates the root item.
+        /// </summary>
+        /// <param name="rootItem">Root item to populate with child items.</param>
+        protected virtual void PopulateRootItem(SidebarItem rootItem)
+        {
+        }
+
+        /// <summary>
+        /// Find item with given id within 
+        /// </summary>
+        /// <param name="id">Id of the item too look for.</param>
+        /// <param name="parent">Parent item where to look item with given id.</param>
+        /// <returns>Sidebar item with requested id if exists, null otherwise.</returns>
+        private static SidebarItem FindById(string id, SidebarItem parent)
+        {
+            foreach (var item in parent.Items)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+
+                var insideNested = FindById(id, item);
+                if (insideNested != null)
+                {
+                    return insideNested;
+                }
+            }
+
+            return null;
+        }
+    }
+}
