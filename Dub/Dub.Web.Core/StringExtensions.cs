80ed3a92af8051a47f44bcb87c2ef44815000694
Andrey Kurdyumov <kant2002@gmail.com>
2015-01-13 15:02:26 +0600

    Add ellipsis builder function

---
+++ Dub/Dub.Web.Core/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StringExtensions.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Core
+{
+    /// <summary>
+    /// Extensions for string manipulation.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Strip string in case if it is longer then specified number.
+        /// </summary>
+        /// <param name="data">String where to put ellipsis.</param>
+        /// <param name="length">Max length of the string.</param>
+        /// <returns>String which is no longer then specified length.</returns>
+        public static string Ellipsis(this string data, int length)
+        {
+            if (data.Length > length)
+            {
+                data = data.Substring(0, length - 1) + "\u2026";
+            }
+
+            return data;
+        }
+    }
+}
