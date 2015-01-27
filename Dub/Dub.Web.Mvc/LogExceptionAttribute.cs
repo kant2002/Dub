c62e3bf0b18ca1d731d4c6fd0155b9364d44837b
Andrey Kurdyumov <kant2002@gmail.com>
2015-01-27 12:24:07 +0600

    Add attribute for capturing log

---
+++ Dub/Dub.Web.Mvc/LogExceptionAttribute.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LogExceptionAttribute.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Mvc
+{
+    using System.Web.Mvc;
+    using Dub.Web.Core;
+
+    /// <summary>
+    /// Represents an attribute that is used to log an exception that is thrown
+    /// by an action method.
+    /// </summary>
+    public class LogExceptionAttribute : FilterAttribute, IExceptionFilter
+    {
+        /// <summary>
+        /// Called when an exception occurs.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.Exception != null)
+            {
+                var routeData = filterContext.RequestContext.RouteData;
+                string controller = routeData.Values["controller"].ToString();
+                string action = routeData.Values["action"].ToString();
+                var userName = filterContext.HttpContext.User.Identity.Name;
+                ExceptionHelper.PublishException(userName, filterContext.Exception);
+            }
+        }
+    }
+}
