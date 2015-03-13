129e57a638ee080d250fb7e62cd0c29f56126a11
Andrey Kurdyumov <kant2002@gmail.com>
2015-03-13 12:30:28 +0600

    Bump version to 0.3.3

--- Dub/CommonAssemblyInfo.cs
+++ Dub/CommonAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Andrey Kurdiumov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.3.3.0")]
+[assembly: AssemblyFileVersion("0.3.3.0")]
