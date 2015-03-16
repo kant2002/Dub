31aec68c51b1cde28881fe97efa0361eb99d64e1
Andrey Kurdyumov <kant2002@gmail.com>
2015-03-16 12:00:12 +0600

    Add generic editing controller

---
+++ Dub/Dub.Web.Core/IGenericStore.cs
@@ -0,0 +1,54 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IGenericStore.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Core
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Generic interface for the persistence store.
+    /// </summary>
+    /// <typeparam name="TEntityKey">Type of key for the entity.</typeparam>
+    /// <typeparam name="TEntity">Type of entity to be persisted.</typeparam>
+    public interface IGenericStore<TEntityKey, TEntity> : IDisposable
+        where TEntity : class
+    {
+        /// <summary>
+        /// Gets entities in the store.
+        /// </summary>
+        IQueryable<TEntity> Items { get; }
+
+        /// <summary>
+        /// Creates a new entity.
+        /// </summary>
+        /// <param name="entity">Message object to be persisted.</param>
+        /// <returns>Task which asynchronously perform operation.</returns>
+        Task CreateAsync(TEntity entity);
+
+        /// <summary>
+        /// Updates existing entity.
+        /// </summary>
+        /// <param name="entity">Message object to be updated.</param>
+        /// <returns>Task which asynchronously perform operation.</returns>
+        Task UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// Finds the entity by it's id.
+        /// </summary>
+        /// <param name="id">Id of the entity to get.</param>
+        /// <returns>Task which asynchronously perform operation.</returns>
+        Task<TEntity> FindByIdAsync(TEntityKey id);
+
+        /// <summary>
+        /// Delete entity.
+        /// </summary>
+        /// <param name="entity">Entity to delete.</param>
+        /// <returns>Task which asynchronously perform operation.</returns>
+        Task DeleteAsync(TEntity entity);
+    }
+}
---
+++ Dub/Dub.Web.Mvc/Controllers/GenericEditController.cs
@@ -0,0 +1,141 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GenericEditController.cs" company="Andrey Kurdiumov">
+// Copyright (c) Andrey Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Dub.Web.Mvc.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    using Dub.Web.Core;
+    using Microsoft.AspNet.Identity.Owin;
+
+    /// <summary>
+    /// Generic controller for editing.
+    /// </summary>
+    /// <typeparam name="TEntityKey">Type of key for the entity.</typeparam>
+    /// <typeparam name="TEntity">Type of entity to be managed.</typeparam>
+    public class GenericEditController<TEntityKey, TEntity> : Controller
+        where TEntity : class, new()
+    {
+        /// <summary>
+        /// Gets persistence store.
+        /// </summary>
+        private GenericStore<TEntityKey, TEntity> Store
+        {
+            get
+            {
+                var context = HttpContext.GetOwinContext();
+                var store = context.Get<GenericStore<TEntityKey, TEntity>>();
+                return store;
+            }
+        }
+
+        /// <summary>
+        /// Displays list of all entities.
+        /// </summary>
+        /// <returns>Result of the action.</returns>
+        public ActionResult Index()
+        {
+            var store = this.Store;
+            return this.View(store.Items);
+        }
+
+        /// <summary>
+        /// Show create entity page.
+        /// </summary>
+        /// <returns>Result of the action.</returns>
+        public ActionResult Create()
+        {
+            var model = new TEntity();
+            return this.View(model);
+        }
+
+        /// <summary>
+        /// Saves changes during creation of the entity.
+        /// </summary>
+        /// <param name="model">Data to use when create entity.</param>
+        /// <returns>Task which returns result of the action.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TEntity model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            await this.Store.CreateAsync(model);
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Show edit entity page by id.
+        /// </summary>
+        /// <param name="id">Id of the entity to get</param>
+        /// <returns>Task which returns result of the action.</returns>
+        public async Task<ActionResult> Edit(TEntityKey id)
+        {
+            var model = await this.Store.FindByIdAsync(id);
+            if (model == null)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(model);
+        }
+
+        /// <summary>
+        /// Saves changes during editing of the user.
+        /// </summary>
+        /// <param name="model">Data to save about user.</param>
+        /// <returns>Task which returns result of the action.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TEntity model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            await this.Store.UpdateAsync(model);
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Show delete prompt page for entity with given id.
+        /// </summary>
+        /// <param name="id">Id of the entity to prompt for deletion</param>
+        /// <returns>Task which returns result of the action.</returns>
+        public async Task<ActionResult> Delete(TEntityKey id)
+        {
+            var model = await this.Store.FindByIdAsync(id);
+            if (model == null)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(model);
+        }
+
+        /// <summary>
+        /// Deletes entity.
+        /// </summary>
+        /// <param name="model">Entity to delete.</param>
+        /// <returns>Task which returns result of the action.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(TEntity model)
+        {
+            await this.Store.DeleteAsync(model);
+            return this.RedirectToAction("Index");
+        }
+    }
+}
