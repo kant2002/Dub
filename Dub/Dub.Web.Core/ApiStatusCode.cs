f1a28b96f98a6aeecc50b8a3ec77adefb9bd4907
Cmouse87 <maarraa@mail.ru>
2017-02-27 17:42:43 +0600

    add new needed code

--- Dub/Dub.Web.Core/ApiStatusCode.cs
+++ Dub/Dub.Web.Core/ApiStatusCode.cs
@@ -70,5 +70,59 @@ namespace Dub.Web.Core
         /// Login for this account is disallowed.
         /// </summary>
         LoginNotAllowedError = RangeAccount + 7,
+        
+        /// <summary>
+        /// Indicating user lockout is not enabled.
+        /// </summary>
+        UserLockoutNotEnabled = RangeAccount + 8,
+        
+        /// <summary>
+        /// Indicating a user already has a password.
+        /// </summary>
+        UserAlreadyHasPassword = RangeAccount + 9,
+        
+        /// <summary>
+        /// Idicating a concurrency failure.
+        /// </summary>
+        ConcurrencyFailure = RangeAccount + 10,
+        
+        /// <summary>
+        /// Idicating an invalid token.
+        /// </summary>
+        InvalidToken = RangeAccount + 11,
+        
+        /// <summary>
+        /// Idicating a password mismatch.
+        /// </summary>
+        PasswordMismatch = RangeAccount + 12,
+        
+        /// <summary>
+        /// Idicating a password entered does not contain a numeric character, 
+        /// which is required by the password policy..
+        /// </summary>
+        PasswordRequiresDigit = RangeAccount + 13,
+        
+        /// <summary>
+        /// Indicating a password entered does not contain a lower case letter, 
+        /// which is required by the password policy.
+        /// </summary>
+        PasswordRequiresLower = RangeAccount + 14,
+        
+        /// <summary>
+        /// Indicating a password entered does not contain a non-alphanumeric character, 
+        /// which is required by the password policy.
+        /// </summary>
+        PasswordRequiresNonAlphanumeric = RangeAccount + 15,
+        
+        /// <summary>
+        /// Indicating a password entered does not contain an upper case letter, 
+        /// which is required by the password policy.
+        /// </summary>
+        PasswordRequiresUpper = RangeAccount + 15,
+        
+        /// <summary>
+        /// Idicating an external login is already associated with an account.
+        /// </summary>
+        LoginAlreadyAssociated = RangeAccount + 16,
     }
 }
